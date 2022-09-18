a206770680d1c6a6734e4633174a68aa903ae88a
«Evgeniia» <girlbysinka@mail.ru>
2022-09-18 14:26:06 +0300

    add hw_task64

---
+++ hw_task64/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N от большего к меньшему.
+M = 1; N = 5. -> ""5, 4, 3, 2, 1""
+M = 4; N = 8. -> ""8, 7, 6, 5, 4""*/
+Console.Clear();
+
+string NumbersRec(int m, int n)
+    {
+        if (m <= n) return NumbersRec(m + 1, n) + $"{m} ";
+        else return String.Empty;
+    }
+
+Console.WriteLine(NumbersRec(1, 10));
